e17042f6746dc7dd4eca8fd4bffabc25a1e2ee74
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2023-11-05 14:09:06 -0800

    Init frontend streaming hook

---
+++ chatgpt/lib/index.ts
@@ -0,0 +1,59 @@
+import { useState, useCallback } from "react";
+
+type QueryStep = {
+  summary?: string;
+  table?: string;
+  sql?: string;
+};
+
+export async function postRaw(
+  url: string,
+  question: string,
+  options?: RequestInit
+) {
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(question),
+    ...options,
+  });
+}
+
+export function useStreamingQuery() {
+  const [querySteps, setQuerySteps] = useState<QueryStep[]>([{}]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const handleTranslate = useCallback(async (question: string) => {
+    try {
+      setIsLoading(true);
+      const response = await postRaw("http://localhost:80000", question);
+      const reader = response.body!.getReader();
+
+      const decoder = (chunk: any) => {
+        const d = new TextDecoder();
+        return d.decode(chunk, { stream: true });
+      };
+
+      let result = [{}];
+
+      while (true) {
+        // eslint-disable-next-line no-await-in-loop
+        const { done, value } = await reader.read();
+        if (done) {
+          setIsLoading(false);
+          break;
+        }
+
+        result.push(decoder(value));
+        setQuerySteps(result);
+      }
+    } catch (e: any) {
+      setError(e);
+    }
+  }, []);
+
+  return { querySteps, trigger: handleTranslate, isLoading, error };
+}
