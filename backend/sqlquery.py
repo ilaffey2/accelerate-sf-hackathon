0aac499275b190d9d17ab6eb7d12391f09f44d89
ian <ian.laffey@mail.mcgill.ca>
2023-11-05 14:58:10 -0800

    fix dollar amount

--- backend/sqlquery.py
+++ backend/sqlquery.py
@@ -9,24 +9,41 @@ load_dotenv()
 
 encoded_credentials = os.getenv("ENCODED_GOOGLE_CREDENTIALS")
 decoded_credentials = base64.b64decode(encoded_credentials).decode("utf-8")
-GCP_CREDS_PATH = './gcp-creds.json'
+GCP_CREDS_PATH = "./gcp-creds.json"
 
 os.makedirs(os.path.dirname(GCP_CREDS_PATH), exist_ok=True)
 
 with open(GCP_CREDS_PATH, "w") as f:
     f.write(decoded_credentials)
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = GCP_CREDS_PATH
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GCP_CREDS_PATH
+
 
 def execute_sql(sql):
     print("Running sql query: \n", sql)
-    
+
     client = bigquery.Client()
     query_job = client.query(sql)
     results = query_job.result()
-    rows = [row.values() for row in results]
-    columns = [Column(name=field.name, type=field.field_type) for field in results.schema]
+
+    columns = [
+        Column(name=field.name, type=field.field_type) for field in results.schema
+    ]
+
+    # Log each column's name and type
+    for column in columns:
+        print(f"Column: {column.name}, Type: {column.type}")
+
+    # Detect if a column is float and format the corresponding rows as money
+    rows = []
+    for row in results:
+        formatted_row = []
+        for i, value in enumerate(row.values()):
+            if columns[i].type == "FLOAT":
+                formatted_row.append("${:,.2f}".format(value))
+            else:
+                formatted_row.append(value)
+        rows.append(formatted_row)
 
     print(results.schema)
     return rows, columns
-
