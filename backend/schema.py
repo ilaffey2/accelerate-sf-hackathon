e8306a4ad433f99bb7c53b87f54b364737e568c5
janruettinger <service+git@janruettinger.com>
2023-11-04 20:40:11 -0700

    add visualization endpoints (wip)

--- backend/schema.py
+++ backend/schema.py
@@ -19,3 +19,6 @@ class Table(BaseModel):
 class QueryResponse(BaseModel):
     summary: str
     table: Table
+
+class VisualizeResponse(BaseModel):
+    imageString: str
--- backend/visualize.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import List
-
-app = FastAPI()
-
-
